196bb5f8824bbf3342dd784cc1f410bf877a6d33
mokhelek <mkatleho191@gmail.com>
2023-07-27 00:31:52 +0200

    add article form

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,10 @@
 from django import forms 
 
 from .models import *
-
+from django_quill.fields import QuillField
+from django_quill.forms import QuillFormField
 
 class ArticleForm(forms.ModelForm):
-
     class Meta:
         model = Articles
         fields = '__all__'
